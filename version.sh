f764a7289730e4a70c756f34a69b30dc1d5665cb
Robert Nelson <robertcnelson@gmail.com>
2015-11-13 20:36:50 -0600

    import script stack
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ version.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+#
+ARCH=$(uname -m)
+
+config="omap2plus_defconfig"
+
+#toolchain="gcc_linaro_eabi_4_8"
+#toolchain="gcc_linaro_eabi_4_9"
+#toolchain="gcc_linaro_eabi_5"
+#toolchain="gcc_linaro_gnueabi_4_6"
+#toolchain="gcc_linaro_gnueabihf_4_7"
+#toolchain="gcc_linaro_gnueabihf_4_8"
+#toolchain="gcc_linaro_gnueabihf_4_9"
+#toolchain="gcc_linaro_gnueabihf_5"
+
+#Kernel/Build
+KERNEL_REL=4.X
+KERNEL_TAG=${KERNEL_REL}
+BUILD=x0
+kernel_rt=".X-rtY"
+
+#v4.X-rcX + upto SHA
+#prev_KERNEL_SHA=""
+#KERNEL_SHA=""
+
+#git branch
+#BRANCH="v4.X.x"
+
+DISTRO=cross
+DEBARCH=armhf
+#
