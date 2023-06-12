ae3c8933861547a237fb5faa323437bea4b45e87
Robert Nelson <robertcnelson@gmail.com>
2023-06-12 15:06:27 -0500

    kernel v6.3.7-arm64-k3-r18.1 rebase external git projects BBDTBS: https://git.beagleboard.org/beagleboard/BeagleBoard-DeviceTrees/-/commit/17a9f0fd6c12cd4d1eccc4fb837c343995f30baf AUFS: https://github.com/sfjro/aufs-standalone/commit/368a74063cab824dc003fc59a828172bdd3a2d1f WIRELESS_REGDB: https://git.kernel.org/pub/scm/linux/kernel/git/sforshee/wireless-regdb.git/commit/?id=9dc0800cdd7aeeae273b983973a1749f73008291
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- patch.sh
+++ patch.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 #
-# Copyright (c) 2009-2022 Robert Nelson <robertcnelson@gmail.com>
+# Copyright (c) 2009-2023 Robert Nelson <robertcnelson@gmail.com>
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
@@ -433,7 +433,7 @@ patch_backports (){
 }
 
 backports () {
-	backport_tag="v5.10.182"
+	backport_tag="v5.10.183"
 
 	subsystem="uio"
 	#regenerate="enable"
@@ -466,7 +466,7 @@ drivers
 packaging () {
 	#do_backport="enable"
 	if [ "x${do_backport}" = "xenable" ] ; then
-		backport_tag="v6.3.6"
+		backport_tag="v6.3.7"
 
 		subsystem="bindeb-pkg"
 		#regenerate="enable"
