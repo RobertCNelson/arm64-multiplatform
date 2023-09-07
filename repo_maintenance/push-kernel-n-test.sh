29823cb09478884cea4bc1c09055e4e27829c978
Robert Nelson <robertcnelson@gmail.com>
2023-09-07 14:12:02 -0500

    cleanup: drop WPANUSB: moving to greybus: Add BeaglePlay Greybus Driver
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- repo_maintenance/push-kernel-n-test.sh
+++ repo_maintenance/push-kernel-n-test.sh
@@ -26,10 +26,6 @@ cat_files () {
 		cat ./patches/external/git/TI_AMX3_CM3 >> ${wfile}
 	fi
 
-	if [ -f ./patches/external/git/WPANUSB ] ; then
-		cat ./patches/external/git/WPANUSB >> ${wfile}
-	fi
-
 	if [ -f ./patches/external/git/BCFSERIAL ] ; then
 		cat ./patches/external/git/BCFSERIAL >> ${wfile}
 	fi
