fd4404d9cc06444adc8f5e1b30c38bb30b8965df
Robert Nelson <robertcnelson@gmail.com>
2023-09-15 18:36:10 -0500

    BeaglePlay: 6.2-rc4 testing
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -44,10 +44,10 @@ toolchain="gcc_12_aarch64"
 
 #Kernel
 KERNEL_REL=6.2
-KERNEL_TAG=${KERNEL_REL}
+KERNEL_TAG=${KERNEL_REL}-rc4
 kernel_rt="-rc7-rt5"
 #Kernel Build
-BUILD=${build_prefix}1.1
+BUILD=${build_prefix}1.2
 
 #v6.X-rcX + upto SHA
 #prev_KERNEL_SHA=""
