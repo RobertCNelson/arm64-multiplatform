82bdeadadbe13adef101473a324d4ad474672f31
Robert Nelson <robertcnelson@gmail.com>
2023-11-21 17:22:31 -0600

    kernel v6.6-rt-arm64-k3-r11.1 rebase external git projects and rt: v6.6-rt15 BBDTBS: https://git.beagleboard.org/beagleboard/BeagleBoard-DeviceTrees/-/commit/cd088547d3ce223679ed7967a36e49e2411e6f17 RT: patch-6.6-rt15.patch.xz WIRELESS_REGDB: https://git.kernel.org/pub/scm/linux/kernel/git/sforshee/wireless-regdb.git/commit/?id=991b1ef696b7a034a5bf001cf31ab7735888c6e1
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -44,10 +44,10 @@ toolchain="gcc_13_aarch64"
 
 #Kernel
 KERNEL_REL=6.6
-KERNEL_TAG=${KERNEL_REL}.1
-kernel_rt="-rc6-rt10"
+KERNEL_TAG=${KERNEL_REL}
+kernel_rt="-rt15"
 #Kernel Build
-BUILD=${build_prefix}11
+BUILD=${build_prefix}11.1
 
 #v6.X-rcX + upto SHA
 #prev_KERNEL_SHA=""
