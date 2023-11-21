fd521b56cc55bcac06e7b0a2bfe871349a200a0b
Robert Nelson <robertcnelson@gmail.com>
2023-11-21 16:29:19 -0600

    kernel v6.5.12-arm64-k3-r8.2 rebase external git projects BBDTBS: https://git.beagleboard.org/beagleboard/BeagleBoard-DeviceTrees/-/commit/23e155ab63feba42e075ce7c6d929b701083516b WIRELESS_REGDB: https://git.kernel.org/pub/scm/linux/kernel/git/sforshee/wireless-regdb.git/commit/?id=991b1ef696b7a034a5bf001cf31ab7735888c6e1
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -44,10 +44,10 @@ toolchain="gcc_13_aarch64"
 
 #Kernel
 KERNEL_REL=6.5
-KERNEL_TAG=${KERNEL_REL}.3
+KERNEL_TAG=${KERNEL_REL}.12
 kernel_rt=".2-rt8"
 #Kernel Build
-BUILD=${build_prefix}8.1
+BUILD=${build_prefix}8.2
 
 #v6.X-rcX + upto SHA
 #prev_KERNEL_SHA=""
