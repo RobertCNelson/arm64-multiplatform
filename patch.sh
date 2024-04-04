a36926db4183c18e34dc44e6defae8983ba40309
Robert Nelson <robertcnelson@gmail.com>
2024-04-04 13:13:35 -0500

    kernel v6.8.3-arm64-k3-r8.1 rebase external git projects BBDTBS: https://openbeagle.org/beagleboard/BeagleBoard-DeviceTrees/-/commit/3635d9dc6a7207481a87fd579e9b384bec810baf WIRELESS_REGDB: https://git.kernel.org/pub/scm/linux/kernel/git/wens/wireless-regdb.git/commit/?id=73529a882f83afa26402b18680025334b0215d92 WPANUSB: https://openbeagle.org/beagleconnect/linux/wpanusb/-/commit/6aa9bf65b9d88a2c9a111e7b4aed03de2be9413d
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- patch.sh
+++ patch.sh
@@ -111,7 +111,7 @@ wpanusb () {
 			rm -rf ./wpanusb || true
 		fi
 
-		${git_bin} clone https://openbeagle.org/beagleconnect/linux/wpanusb --depth=1
+		${git_bin} clone https://openbeagle.org/beagleconnect/linux/wpanusb.git --depth=1
 		cd ./wpanusb
 			wpanusb_hash=$(git rev-parse HEAD)
 		cd -
@@ -429,7 +429,7 @@ patch_backports () {
 }
 
 backports () {
-	backport_tag="v5.10.213"
+	backport_tag="v5.10.214"
 
 	subsystem="uio"
 	#regenerate="enable"
@@ -465,7 +465,6 @@ drivers () {
 	dir 'soc/ti/pcie'
 	dir 'boris'
 	dir 'mikrobus'
-	dir 'drivers/sdhci-omap'
 }
 
 ###
@@ -476,7 +475,7 @@ packaging () {
 	echo "Update: package scripts"
 	#do_backport="enable"
 	if [ "x${do_backport}" = "xenable" ] ; then
-		backport_tag="v6.8.1"
+		backport_tag="v6.8.3"
 
 		subsystem="bindeb-pkg"
 		#regenerate="enable"
