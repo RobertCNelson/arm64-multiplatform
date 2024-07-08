c675da8987ce5d86abc0314966cf94ee8170f24a
Robert Nelson <robertcnelson@gmail.com>
2024-07-08 11:41:54 -0500

    rt: patch-6.10-rc6-rt11.patch.xz
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -48,9 +48,9 @@ toolchain="gcc_13_aarch64"
 #Kernel
 KERNEL_REL=6.10
 KERNEL_TAG=${KERNEL_REL}-rc6
-kernel_rt="-rc6-rt10"
+kernel_rt="-rc6-rt11"
 #Kernel Build
-BUILD=${build_prefix}3.1
+BUILD=${build_prefix}3.2
 
 #v6.X-rcX + upto SHA
 #prev_KERNEL_SHA=""
