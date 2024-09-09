71c5e0d33c70a8859e862965025b96847981fdaf
Robert Nelson <robertcnelson@gmail.com>
2024-09-09 13:33:11 -0500

    kernel v6.11-rc7-arm64-k3-r5.1 rebase external git projects BBDTBS: https://openbeagle.org/beagleboard/BeagleBoard-DeviceTrees/-/commit/cf68007bbcb3ff48f91eaed6f1f64419b89cd4ed WIRELESS_REGDB: https://git.kernel.org/pub/scm/linux/kernel/git/wens/wireless-regdb.git/commit/?id=bf55ed481432cbd91e05b54c5f7bccd687c69bb7 WPANUSB: https://openbeagle.org/beagleconnect/linux/wpanusb/-/commit/6aa9bf65b9d88a2c9a111e7b4aed03de2be9413d
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- patch.sh
+++ patch.sh
@@ -111,6 +111,12 @@ external_git () {
 	${git_bin} describe
 }
 
+mainline_patches () {
+	#exit 2
+	dir 'rfc/mainline'
+	#exit 2
+}
+
 wpanusb () {
 	#regenerate="enable"
 	if [ "x${regenerate}" = "xenable" ] ; then
@@ -216,12 +222,6 @@ wireless_regdb () {
 		cleanup
 	fi
 	dir 'external/wireless_regdb'
-}
-
-mainline_patches () {
-	#exit 2
-	dir 'rfc/mainline'
-	#exit 2
 }
 
 cleanup_dts_builds () {
@@ -380,10 +380,10 @@ local_patch () {
 
 copy_mainline_driver
 #external_git
+mainline_patches
 wpanusb
 rt
 wireless_regdb
-mainline_patches
 beagleboard_dtbs
 #local_patch
 
@@ -464,8 +464,6 @@ backports () {
 	else
 		patch_backports
 	fi
-
-	${git} "${DIR}/patches/backports/mmc-core-quirks/0002-mmc-core-apply-SD-quirks-earlier-during-probe.patch"
 }
 
 drivers () {
