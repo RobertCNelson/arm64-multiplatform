8d3c06295c4918bb30bd7638236577345c44195b
Robert Nelson <robertcnelson@gmail.com>
2024-11-18 10:39:10 -0600

    kernel v6.12-arm64-k3-r4.1 rebase external git projects BBDTBS: https://openbeagle.org/beagleboard/BeagleBoard-DeviceTrees/-/commit/42d4cbfc814df54d63346df2dd0522014530f7aa WIRELESS_REGDB: https://git.kernel.org/pub/scm/linux/kernel/git/wens/wireless-regdb.git/commit/?id=b66b9a188adf01b8a7654b902749ae161b5ebc60
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- tools/config-hot-fix.sh
+++ tools/config-hot-fix.sh
@@ -281,6 +281,13 @@ config="CONFIG_UIO_PDRV_GENIRQ" ; config_module
 ./scripts/config --module CONFIG_HX9023S
 ./scripts/config --module CONFIG_AW96103
 
+#new in debian v6.12.x 6.12~rc6-1~exp1
+./scripts/config --enable CONFIG_ZONE_DEVICE
+./scripts/config --module CONFIG_IP_VS_TWOS
+./scripts/config --module CONFIG_VIDEO_OV5648
+./scripts/config --enable CONFIG_DRM_DISPLAY_DP_AUX_CHARDEV
+./scripts/config --module CONFIG_TI_PRUSS
+
 #configure CONFIG_EXTRA_FIRMWARE
 ./scripts/config --set-str CONFIG_EXTRA_FIRMWARE "regulatory.db regulatory.db.p7s cadence/mhdp8546.bin"
 
