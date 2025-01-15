d7f012d1bc8ff9b1ae8f02accd1af0a0b7224352
Robert Nelson <robertcnelson@gmail.com>
2025-01-15 15:37:57 -0600

    kernel v6.11.11-arm64-k3-r9.1 rebase external git projects BBDTBS: https://openbeagle.org/beagleboard/BeagleBoard-DeviceTrees/-/commit/41aec3047c2b01b5adf47b76e932e8288ee8d5e8 WIRELESS_REGDB: https://git.kernel.org/pub/scm/linux/kernel/git/wens/wireless-regdb.git/commit/?id=f67f40d009199ee75c906f1ac898b38be981a7f7
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- patch.sh
+++ patch.sh
@@ -96,14 +96,6 @@ cherrypick () {
 	num=$(($num+1))
 }
 
-copy_mainline_driver () {
-	#regenerate="enable"
-	if [ "x${regenerate}" = "xenable" ] ; then
-		cp -v ./drivers/mmc/core/quirks.h ../patches/mainline/mmc/
-		exit 2
-	fi
-}
-
 external_git () {
 	git_tag=""
 	echo "pulling: ${git_tag}"
@@ -247,7 +239,7 @@ k3_makefile_patch_cleanup_overlays () {
 	echo "# Enable support for device-tree overlays" >> arch/arm64/boot/dts/ti/Makefile
 	cat arch/arm64/boot/dts/ti/Makefile.dtc >> arch/arm64/boot/dts/ti/Makefile
 	rm arch/arm64/boot/dts/ti/Makefile.dtc
-	echo "DTC_FLAGS_k3-am625-pocketbeagle2 += -@" >> arch/arm64/boot/dts/ti/Makefile
+	echo "DTC_FLAGS_k3-am6232-pocketbeagle2 += -@" >> arch/arm64/boot/dts/ti/Makefile
 	echo "DTC_FLAGS_k3-am67a-beagley-ai += -@" >> arch/arm64/boot/dts/ti/Makefile
 	echo "DTC_FLAGS_k3-j721e-beagleboneai64 += -@" >> arch/arm64/boot/dts/ti/Makefile
 }
@@ -301,7 +293,7 @@ beagleboard_dtbs () {
 		device="BONE-I2C2" ; k3_dtbo_makefile_append
 		device="BONE-I2C3" ; k3_dtbo_makefile_append
 
-		device="k3-am625-pocketbeagle2.dtb" ; k3_dtb_makefile_append
+		device="k3-am6232-pocketbeagle2.dtb" ; k3_dtb_makefile_append
 
 		#ls src/arm64/overlays/ | grep beaglebone
 
@@ -342,7 +334,6 @@ local_patch () {
 	${git} "${DIR}/patches/dir/0001-patch.patch"
 }
 
-copy_mainline_driver
 #external_git
 mainline_patches
 rt
@@ -431,8 +422,6 @@ backports () {
 
 drivers () {
 	dir 'branding/boris'
-
-	dir 'drivers/pcie'
 
 	dir 'external/cadence'
 	dir 'external/gasket'
