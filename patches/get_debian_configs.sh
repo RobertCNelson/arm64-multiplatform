a0ade11f7dc9e80313e275e9eed2bc58c02841e4
Robert Nelson <robertcnelson@gmail.com>
2025-04-09 16:18:10 -0500

    kernel v6.12.22-arm64-k3-r18.1 rebase external git projects BBDTBS: https://openbeagle.org/beagleboard/BeagleBoard-DeviceTrees/-/commit/63e5f2cab456eeb64536539a96ce14d075d13346 WIRELESS_REGDB: https://git.kernel.org/pub/scm/linux/kernel/git/wens/wireless-regdb.git/commit/?id=6953f19b96f4bf21b22de5c039e372bb33e2c96b
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- patches/get_debian_configs.sh
+++ patches/get_debian_configs.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
 #
-#https://packages.debian.org/source/sid/linux
+#https://packages.debian.org/source/trixie/linux
 #
-abi="6.12.9"
-kernel="6.12.9-1"
-#
-#abi="6.12-rc6"
-#kernel="6.12~rc6-1~exp1"
+abi="6.12.21"
+kernel="6.12.21-1"
 #
 
 mirror_site="http://192.168.1.10/debian/pool/main/l/linux"
