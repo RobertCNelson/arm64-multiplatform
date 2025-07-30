f7d364d709194c2eda6fbfad976cbf59938fbb0f
Robert Nelson <robertcnelson@gmail.com>
2025-07-30 13:44:37 -0500

    kernel v6.12.40-arm64-k3-r26.2 rebase external git projects BBDTBS: https://github.com/beagleboard/BeagleBoard-DeviceTrees/commit/83e79b04b2666fe7b847909fe346470d50b7999a WIRELESS_REGDB: https://git.kernel.org/pub/scm/linux/kernel/git/wens/wireless-regdb.git/commit/?id=e05260afb7203dd0a7ba6faf9a30c86faf19a24e
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- patch.sh
+++ patch.sh
@@ -469,7 +469,7 @@ post_rpibackports () {
 }
 
 backports () {
-	backport_tag="v6.16-rc5"
+	backport_tag="v6.16"
 
 	subsystem="tps65219"
 	#regenerate="enable"
@@ -512,8 +512,6 @@ drivers () {
 	dir 'external/android'
 	dir 'external/cadence'
 	dir 'external/gasket'
-
-	dir 'drivers/tps65219'
 }
 
 ###
