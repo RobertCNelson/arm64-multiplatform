0b4e87f59c92596dd82396502a81cbc196601b0b
Robert Nelson <robertcnelson@gmail.com>
2025-10-14 16:33:19 -0500

    kernel v6.17.2-arm64-k3-r8.1 rebase external git projects BBDTBS: https://github.com/beagleboard/BeagleBoard-DeviceTrees/commit/a95c55000d578a0918f38f4a03e052b01babbac1 WIRELESS_REGDB: https://git.kernel.org/pub/scm/linux/kernel/git/wens/wireless-regdb.git/tag/?h=master-2025-10-07
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ scripts/pull.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+git pull --no-edit https://github.com/RobertCNelson/arm64-multiplatform.git v6.17.x-arm64-k3
+git pull --no-edit https://gitlab.com/RobertCNelson/arm64-multiplatform.git v6.17.x-arm64-k3
+
