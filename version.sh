ff752468ec6222bc01f40bf2d31deea138f1c92c
Robert Nelson <robertcnelson@gmail.com>
2025-10-28 16:01:28 -0500

    kernel v6.12.55-arm64-k3-r33.1 rebase external git projects BBDTBS: https://github.com/beagleboard/BeagleBoard-DeviceTrees/commit/e3a3a2b360f47886ec06308df0b7514b35601358 WIRELESS_REGDB: https://git.kernel.org/pub/scm/linux/kernel/git/wens/wireless-regdb.git/tag/?h=master-2025-10-07
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -54,11 +54,11 @@ linux_repo="https://kernel.googlesource.com/pub/scm/linux/kernel/git/torvalds/li
 linux_stable_repo="https://kernel.googlesource.com/pub/scm/linux/kernel/git/stable/linux.git"
 #
 KERNEL_REL=6.12
-KERNEL_TAG=${KERNEL_REL}.54
+KERNEL_TAG=${KERNEL_REL}.55
 #https://mirrors.edge.kernel.org/pub/linux/kernel/projects/rt/6.12/
 kernel_rt=".49-rt13"
 #Kernel Build
-BUILD=${build_prefix}33
+BUILD=${build_prefix}33.1
 
 #v6.X-rcX + upto SHA
 #prev_KERNEL_SHA=""
