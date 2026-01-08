c58d8d09d04b429ce4f8550dba0b550c76afd836
Robert Nelson <robertcnelson@gmail.com>
2026-01-08 13:56:45 -0600

    kernel v6.6.119-rt-arm64-k3-r46.1 rebase external git projects and rt: v6.6.116-rt66 BBDTBS: https://github.com/beagleboard/BeagleBoard-DeviceTrees/commit/5020b311d32e8dedc7f853c8d746fb349bbdd6ad RT: patch-6.6.116-rt66.patch.xz WIRELESS_REGDB: https://git.kernel.org/pub/scm/linux/kernel/git/wens/wireless-regdb.git/tag/?h=master-2025-10-07
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- tools/config-hot-fix.sh
+++ tools/config-hot-fix.sh
@@ -411,4 +411,6 @@ cd ${DIR}/KERNEL/
 ./scripts/config --module CONFIG_LEDS_CLASS_FLASH
 ./scripts/config --module CONFIG_GREYBUS_LIGHT
 
+./scripts/config --module CONFIG_MWIFIEX_SDIO
+
 cd ${DIR}/
