116f7a38fcfd22342b8dd4f32351e167bb4b8baf
Robert Nelson <robertcnelson@gmail.com>
2026-04-27 16:45:32 -0500

    kernel v6.18.25-arm64-k3-r32.3 rebase external git projects BBDTBS: https://github.com/beagleboard/BeagleBoard-DeviceTrees/commit/a2585fa1c97f59cfa630cb05d30a7cc9bff6773e WIRELESS_REGDB: https://git.kernel.org/pub/scm/linux/kernel/git/wens/wireless-regdb.git/tag/?h=master-2026-03-18
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- patch.sh
+++ patch.sh
@@ -516,6 +516,19 @@ backports () {
 	fi
 }
 
+cc33xx_drivers () {
+	echo "dir: drivers/cc33xx/v5_20241107"
+	#b4 am https://lore.kernel.org/linux-wireless/20241107125209.1736277-1-michael.nemanov@ti.com/
+	${git} "${DIR}/patches/drivers/cc33xx/v5_20241107/v5_20241107_michael_nemanov_wifi_cc33xx_add_driver_for_new_ti_cc33xx_wireless_device_family.mbx"
+
+	#exit 2
+	#start_cleanup
+	dir 'drivers/cc33xx/1.0.2.10'
+
+	#exit 2
+#	dir 'drivers/cc33xx/fixes'
+}
+
 drivers () {
 	dir 'branding/boris'
 
@@ -534,15 +547,12 @@ drivers () {
 	#git am ./v2_20260114_tomeu_new_drm_accel_driver_for_texas_instruments_c7x_dsps.mbx
 	#exit 2
 	dir 'drivers/thames'
-
-	echo "dir: drivers/cc33xx-b4"
-	#b4 am https://lore.kernel.org/linux-wireless/20241107125209.1736277-1-michael.nemanov@ti.com/
-	${git} "${DIR}/patches/drivers/cc33xx-b4/v5_20241107_michael_nemanov_wifi_cc33xx_add_driver_for_new_ti_cc33xx_wireless_device_family.mbx"
 }
 
 ###
 backports
 drivers
+cc33xx_drivers
 
 echo "patch.sh ran successfully"
 #
