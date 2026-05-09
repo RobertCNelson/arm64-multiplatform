43795aa3bb6f6e5d6b0a3627baa360715897730b
Robert Nelson <robertcnelson@gmail.com>
2026-05-09 09:36:33 -0500

    debian: 7.0.4
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- tools/get_debian_configs.sh
+++ tools/get_debian_configs.sh
@@ -7,14 +7,17 @@
 #
 #https://packages.debian.org/source/sid/linux
 #
-abi="7.0"
-kernel="7.0.3-1"
+trixie_kernel_branch="6.12"
+trixie_kernel_tag="6.12.86-1"
 #
-#abi="7.0"
-#kernel="7.0.1-1~exp1"
+forky_kernel_branch="6.19"
+forky_kernel_tag="6.19.14-1"
 #
-#abi="6.19"
-#kernel="6.19~rc8-1~exp1"
+sid_kernel_branch="7.0"
+sid_kernel_tag="7.0.4-1"
+#
+exp_kernel_branch="7.1"
+exp_kernel_tag="7.1~rc2-1~exp1"
 #
 
 mirror_site="http://192.168.1.10/debian/pool/main/l/linux"
@@ -23,40 +26,46 @@ debian_security_site="http://deb.debian.org/debian-security/pool/main/l/linux"
 incoming_site="http://incoming.debian.org/debian-buildd/pool/main/l/linux"
 
 dl_deb () {
-	if [ ! -f ./dl/linux-config-${abi}_${kernel}_${dpkg_arch}.deb ] ; then
-		wget -cq --directory-prefix=./dl/ ${mirror_site}/linux-config-${abi}_${kernel}_${dpkg_arch}.deb
+	if [ ! -f ./dl/linux-config-${kernel_branch}_${kernel_tag}_${dpkg_arch}.deb ] ; then
+		wget -cq --directory-prefix=./dl/ ${mirror_site}/linux-config-${kernel_branch}_${kernel_tag}_${dpkg_arch}.deb
 	fi
 
-	if [ ! -f ./dl/linux-config-${abi}_${kernel}_${dpkg_arch}.deb ] ; then
-		wget -cq --directory-prefix=./dl/ ${debian_site}/linux-config-${abi}_${kernel}_${dpkg_arch}.deb
+	if [ ! -f ./dl/linux-config-${kernel_branch}_${kernel_tag}_${dpkg_arch}.deb ] ; then
+		wget -cq --directory-prefix=./dl/ ${debian_site}/linux-config-${kernel_branch}_${kernel_tag}_${dpkg_arch}.deb
 	fi
 
-	if [ ! -f ./dl/linux-config-${abi}_${kernel}_${dpkg_arch}.deb ] ; then
-		wget -cq --directory-prefix=./dl/ ${incoming_site}/linux-config-${abi}_${kernel}_${dpkg_arch}.deb
+	if [ ! -f ./dl/linux-config-${kernel_branch}_${kernel_tag}_${dpkg_arch}.deb ] ; then
+		wget -cq --directory-prefix=./dl/ ${incoming_site}/linux-config-${kernel_branch}_${kernel_tag}_${dpkg_arch}.deb
 	fi
 
-	if [ ! -f ./dl/linux-config-${abi}_${kernel}_${dpkg_arch}.deb ] ; then
-		wget -cq --directory-prefix=./dl/ ${debian_security_site}/linux-config-${abi}_${kernel}_${dpkg_arch}.deb
+	if [ ! -f ./dl/linux-config-${kernel_branch}_${kernel_tag}_${dpkg_arch}.deb ] ; then
+		wget -cq --directory-prefix=./dl/ ${debian_security_site}/linux-config-${kernel_branch}_${kernel_tag}_${dpkg_arch}.deb
 	fi
 
-	if [ -f ./dl/linux-config-${abi}_${kernel}_${dpkg_arch}.deb ] ; then
-		echo "[linux-config-${abi}_${kernel}_${dpkg_arch}.deb]"
-		dpkg -x ./dl/linux-config-${abi}_${kernel}_${dpkg_arch}.deb ./dl/tmp/
-		if [ -f ./dl/tmp/usr/src/linux-config-${abi}/config.${dpkg_arch}_${config}.xz ] ; then
-			xzcat -v ./dl/tmp/usr/src/linux-config-${abi}/config.${dpkg_arch}_${config}.xz > ./debian.config
+	if [ -f ./dl/linux-config-${kernel_branch}_${kernel_tag}_${dpkg_arch}.deb ] ; then
+		echo "[linux-config-${kernel_branch}_${kernel_tag}_${dpkg_arch}.deb]"
+		dpkg -x ./dl/linux-config-${kernel_branch}_${kernel_tag}_${dpkg_arch}.deb ./dl/tmp/
+		if [ -f ./dl/tmp/usr/src/linux-config-${kernel_branch}/config.${dpkg_arch}_${config}.xz ] ; then
+			xzcat -v ./dl/tmp/usr/src/linux-config-${kernel_branch}/config.${dpkg_arch}_${config}.xz > ./patches/debian.config
 		else
-			tree ./dl/tmp/usr/src/linux-config-${abi}/
+			tree ./dl/tmp/usr/src/linux-config-${kernel_branch}/
 			exit 2
 		fi
 		rm -rf ./dl/tmp/ || true
 	else
-		echo "[linux-config-${abi}_${kernel}_${dpkg_arch}.deb] NOT BUILT YET"
+		echo "[linux-config-${kernel_branch}_${kernel_tag}_${dpkg_arch}.deb] NOT BUILT YET"
 	fi
 }
 
-dpkg_arch="arm64"
-config="none_arm64"
-dl_deb
+dl_distro () {
+	dpkg_arch="arm64"
+	config="none_arm64"
+	dl_deb
+}
+
+kernel_branch="${sid_kernel_branch}"
+kernel_tag="${sid_kernel_tag}"
+dl_distro
 
 rm -rf ./dl/ || true
 
