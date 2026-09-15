edb858da66352d9017d19f09656736f7a7da6d5d
Robert Nelson <robertcnelson@gmail.com>
2026-09-15 08:46:44 -0500

    kernel v6.6.156-rt-arm64-k3-r62.1 rebase external git projects and rt: v6.6.156-rt79 BBDTBS: https://github.com/beagleboard/BeagleBoard-DeviceTrees/commit/673295321130592541fb7b5f27b8776afd04e3df RT: patch-6.6.151-rt78.patch.xz WIRELESS_REGDB: https://git.kernel.org/pub/scm/linux/kernel/git/wens/wireless-regdb.git/tag/?h=master-2026-09-03
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- scripts/pull.sh
+++ scripts/pull.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 # SPDX-FileCopyrightText: Robert Nelson <robertcnelson@gmail.com>
-#
 # SPDX-License-Identifier: MIT
 
-git pull --no-edit https://github.com/RobertCNelson/arm64-multiplatform.git v6.6.x-rt-arm64-k3
-git pull --no-edit https://gitlab.com/RobertCNelson/arm64-multiplatform.git v6.6.x-rt-arm64-k3
+BRANCH="v6.6.x-rt-arm64-k3"
+REPO="arm64-multiplatform"
 
+git pull --no-edit https://github.com/RobertCNelson/${REPO}.git ${BRANCH}
+git pull --no-edit https://gitlab.com/RobertCNelson/${REPO}.git ${BRANCH}
