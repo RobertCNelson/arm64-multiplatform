c05e2714b78f6761dd897b5457727500aefb2a5f
Robert Nelson <robertcnelson@gmail.com>
2026-09-22 17:25:00 -0500

    6.18.53-arm64-k3-r57 release BBDTBS: https://github.com/beagleboard/BeagleBoard-DeviceTrees/commit/749844d99346b81344a77bdce8695a82eab7faef CC33XX_FIRMWARE: https://www.ti.com/tool/download/CC33XX-LINUX-MPU/1.0.2.10 WIRELESS_REGDB: https://git.kernel.org/pub/scm/linux/kernel/git/wens/wireless-regdb.git/tag/?h=master-2026-09-03 FIRMWARE_TAG: https://git.kernel.org/pub/scm/linux/kernel/git/firmware/linux-firmware.git/log/powervr?h=20260916
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -67,7 +67,7 @@ KERNEL_TAG=${KERNEL_REL}.53
 #https://mirrors.edge.kernel.org/pub/linux/kernel/projects/rt/x.y/
 kernel_rt=".X-rtY"
 #Kernel Build
-BUILD=${build_prefix}56.1
+BUILD=${build_prefix}57
 
 #git branch
 BRANCH="${branch_prefix}${KERNEL_REL}${branch_postfix}"
